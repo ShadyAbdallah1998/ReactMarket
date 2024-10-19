18ea1cdbddb5fb4a412ec325f9bb97d1e0a4573f
ShadyAbdallah <ShadyAbdallahG98@gmail.com>
2024-10-19 18:53:58 +0300

    Editing bg of the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   const randomData = dynamicName[changeString(2)];
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center my-5 text-amber-600">
+      <h1 className="text-3xl font-bold text-center my-5 text-amber-600 bg-slate-300 py-3">
         {randomData} Store
       </h1>
     </div>
